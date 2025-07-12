a54deeeecb532ad81ed37ff91aaa55f95efad853
Sallieu-99 <sallieusesay441@gmail.com>
2025-07-12 12:02:27 +0200

    Update Output.tf

--- .github/workflows/Bootstrap-backend/Output.tf
+++ .github/workflows/Bootstrap-backend/Output.tf
@@ -7,8 +7,5 @@ output "table" {
   value = aws_dynamodb_table.terraform_locks.id
 } 
 
-output "public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.example.public_ip
-}
+
 
