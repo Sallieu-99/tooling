e07c1374f59983dbdfe8221ae3b8be42f08678ba
Sallieu-99 <sallieusesay441@gmail.com>
2025-07-12 12:03:32 +0200

    Create backend.tf

---
+++ .github/workflows/Bootstrap-backend/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "class38-terraform-backend-bucket-project-edna"
+    key            = "tooling/s3/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+}
