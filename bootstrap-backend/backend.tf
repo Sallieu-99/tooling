16e617ed038cd92befec09b92c7429f68a11dfc7
Sallieu-99 <sallieusesay441@gmail.com>
2025-10-16 19:23:03 +0200

    Change S3 bucket name for Terraform backend

--- bootstrap-backend/backend.tf
+++ bootstrap-backend/backend.tf
@@ -1,7 +1,7 @@
 /*
 terraform {
   backend "s3" {
-    bucket         = "class38-terraform-backend-bucket-project-edna"
+    bucket         = "class38-terraform-backend-bucket-project-skala"
     key            = "tooling/s3/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
